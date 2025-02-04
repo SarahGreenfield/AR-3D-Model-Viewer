d4bd0ccc724781998512c8e4a266bae6ca9e081b
SarahGreenfield <sarah.greenfield@csu.fullerton.edu>
2025-02-04 15:48:28 -0800

    imported libraries and tested if everything runs.

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2 #openCV Library imported
+import pyvista as pv #pyvista used for being able to display .obj files
+import vtk #another open-source library for 3D graphics. 
+
+#the main function will be defined here
+def main(): 
+    print()
+   # print("Hello") #test if everything is running.
+
+
+#this is where the overall function will be called
+main()
