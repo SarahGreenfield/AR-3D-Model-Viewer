f1a71ac94c8d07aa288992829eba4fc1a8dc64e3
SarahGreenfield <sarah.greenfield@csu.fullerton.edu>
2025-03-12 13:15:43 -0700

    Added user input and a way for them to exit out of the program with options to continue. and more comments :)

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import open3d as o3d #importing open3D also
 #the function to get the object file
 def getObject():
     print()
+    #user input, the user is prompted to type in the path to their Object file
+    #that input will be stored and returned to be used in other functions
     #myObject = o3d.data.ArmadilloMesh() #simply testing to ensure that the code will work
-    myObject = 'C:/Users/sarah/Desktop/3D_Models/Goat skull.ply' #hard-coding the path here for now
+    #myObject = 'C:/Users/sarah/Desktop/3D_Models/Goat skull.ply' #hard-coding the path here for now
+    #print("Enter")
+    myObject = input("Enter the path to your 3D model file: ") #prompts the user to enter the path to the 3D object file they want to view.
     return myObject #so this variable can be used in other functions
 
 #enable camera
 def enableCamera():
     print()
+    #will be testing out OpenCV here and maybe have the camera background needed when displaying the 3d object
 
 #User input
 def userControls():
@@ -35,10 +40,19 @@ def displayObject(x):
 
 #the main function will be defined here
 def main(): 
-    print()
+    print("Hello! Welcome to the 3D Object Viewer!")
    # print("Hello") #test if everything is running.
-    userObject = getObject()
-    displayObject(userObject)
+    print()
+    userInput = input("Select what you want: (s to show, q to quit)")
+    while userInput != 'q':  #while loop so the user can decide to view another object or exit    
+        if userInput == 's': #for now it is a specific key for now. Right now, displaying the 3D model is a success! Now to try my hand at OpenCV
+            userObject = getObject()
+            displayObject(userObject)
+        userInput = input("see another?: (q to quit)")
+    
+    print("thank you and have a nice day!")
+    quit
+    
 
 
 #this is where the overall function will be called
