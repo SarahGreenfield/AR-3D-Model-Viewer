2c0b5d042e6dfc6c57b8396e7549af5853e7fe87
SarahGreenfield <sarah.greenfield@csu.fullerton.edu>
2025-03-24 16:22:03 -0700

    Added (in comments for now) the PyVista method to view the object, and got the function to enable the camera. Now only to have the camera feed be the background when displaying the object we want to view.

--- main.py
+++ main.py
@@ -3,24 +3,49 @@ import pyvista as pv #pyvista used for being able to display .obj files
 import vtk #another open-source library for 3D graphics. necessary for stl files
 import open3d as o3d #importing open3D also
 
+WEBCAM = cv2.VideoCapture(0) #camera constant to be used in any function
+#user input path example: C:/Users/sarah/Desktop/3D_Models/DeathStar.stl
+
 #the function to get the object file
 def getObject():
     print()
     #user input, the user is prompted to type in the path to their Object file
     #that input will be stored and returned to be used in other functions
     #myObject = o3d.data.ArmadilloMesh() #simply testing to ensure that the code will work
-    #myObject = 'C:/Users/sarah/Desktop/3D_Models/Goat skull.ply' #hard-coding the path here for now
+    #myObject = 'C:/Users/sarah/Desktop/3D_Models/DeathStar.stl' #hard-coding the path here for now
     #print("Enter")
     myObject = input("Enter the path to your 3D model file: ") #prompts the user to enter the path to the 3D object file they want to view.
     return myObject #so this variable can be used in other functions
 
 #enable camera
+#function for the camera will be changed since the while loop is within this function
 def enableCamera():
     print()
     #will be testing out OpenCV here and maybe have the camera background needed when displaying the 3d object
+    #using the youtube tutorial as reference
+    camera = cv2.VideoCapture(0) #just accessing the webcam, not reading a video file
+    #if statement for if there is no camera connected
+    if not camera.isOpened():
+        print("No Camera Identified. Please check your connection with the webcam/camera.")
+        exit()
+    
+    #with a camera
+    print("press 'x' to turn off the camera")
+    while(True):
+        ret, frame = camera.read()
+        if not ret:
+                print("no more video")
+                break
+        cv2.imshow("Object Viewer", frame)
+        if cv2.waitKey(1) == ord('x'):
+            break
+        
+    camera.release()
+    cv2.destroyAllWindows()
+    
 
 #User input
-def userControls():
+def userControls(): #for use of arrow keys for control of the object (most likely going to be optional, since this one will be viewed on the computer and we already have access to the mouse to control it)
     print()
 
 #to display the object using TriangleMesh
@@ -29,12 +54,18 @@ def displayObject(x):
     #if user is using .stl:
 
     #else, use o3d
+    
     mesh = o3d.io.read_triangle_mesh(x)
     #mesh = o3d.io.read_point_cloud("DeathStar.ply")s
     mesh.compute_vertex_normals() # more info on that
     o3d.visualization.draw_geometries([mesh])
-
+    
     #print(mesh)
+    #PyVista Code Here!
+    '''
+    mesh = pv.read(x)
+    mesh.plot(background='black', show_edges=True) 
+    '''
 
 
 
@@ -45,8 +76,13 @@ def main():
     print()
     userInput = input("Select what you want: (s to show, q to quit)")
     while userInput != 'q':  #while loop so the user can decide to view another object or exit    
-        if userInput == 's': #for now it is a specific key for now. Right now, displaying the 3D model is a success! Now to try my hand at OpenCV
+        if userInput != 'q': #for now it is a specific key for now. Right now, displaying the 3D model is a success! Now to try my hand at OpenCV
             userObject = getObject()
+            #enableCamera()
+            #add the loop for the camera here and see what happens
+            
+            
+            
             displayObject(userObject)
         userInput = input("see another?: (q to quit)")
     
